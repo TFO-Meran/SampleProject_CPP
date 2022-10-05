53b68edd9f26e98b51ab97be54673368b81eeb2b
cg <c.g.mail@gmx.net>
2022-10-05 11:09:11 +0200

    create onefile branch..

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 /******************************************************************************
  *   
- *   @file:  main.cpp
- *   @brief:  sample code with simple "pass by reference"-funcion
- *   @author: CG
- *   Website:  https://...
- *   Support email:  ...
+ *   @file:     main.cpp
+ *   @brief:    sample code with simple "pass by reference"-funcion
+ *   @author:   CG
+ *   Website:   https://...
+ *   Support:   ...
  *   @todo: ..test todo.. 
  *     
  ******************************************************************************/
@@ -20,29 +20,60 @@
 // ****************************************************************************
 
 #include <iostream>
-#include "test.h"
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
-//=============================================================================
-//                           MAIN
-//=============================================================================
-int main(int, char **)
-{
-    cout << "Hello World!\n";
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+int potenz(int a, int b);
 
-    int a;
-    int b;
+int main() {
+    string wdh="";
+    
+    do {
+        int basis, exponent, ergebnis, i;
+        
+        cout << "Geben Sie die Basis ein:" << endl;
+        cin >> basis;
+        cout << "Geben Sie den Exponenten ein:" << endl;
+        cin >> exponent;
+        cout << basis << " hoch " << exponent << " ergibt " << potenz(basis, exponent) << endl;
+        cout << "[Enter] = wiederholen / [q] = beenden" << endl;
+        cin >> wdh;
+    } while (wdh != "q");
+    return 0;
+}
 
+int potenz(int a, int b) {
+    int c, i;
+    
+    c = 1;
+    for (i = 1; i <= b; i++) {
+        c = c * a;
+    }
+    
+    return c;
+}
 
-    cout << "Switch variables!\n";
-    cout << "Enter a value a:\n";
-    cin >> a;
-    cout << "Enter a value b:\n";
-    cin >> b;
-    cout << "a=" << a << " b=" << b << "\n";
-    switchval(a, b);
-    cout << "a=" << a << " b=" << b << "\n";
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
 }
 
 //=============================================================================
