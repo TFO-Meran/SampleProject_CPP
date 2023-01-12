910a12fe57efacdf9f549dd74d2d8fd2719995d4
cg <c.g.mail@gmx.net>
2023-01-12 18:24:11 +0100

    modified cmakelists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@
 // ****************************************************************************
 
 #include <iostream>
-#include "test.h"
 
 using namespace std;
 
@@ -27,19 +26,7 @@ using namespace std;
 int main(int, char **)
 {
     cout << "Hello World!\n";
-
-    int a;
-    int b;
-
-
-    cout << "Switch variables!\n";
-    cout << "Enter a value a:\n";
-    cin >> a;
-    cout << "Enter a value b:\n";
-    cin >> b;
-    cout << "a=" << a << " b=" << b << "\n";
-    switchval(a, b);
-    cout << "a=" << a << " b=" << b << "\n";
+    return 0;
 }
 
 //=============================================================================
